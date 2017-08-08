a84e3e7014a2dc35ddfdd006540ff9cccdc9ad51
zp <1162851277@qq.com>
2017-08-08 14:48:21 +0800

    Initial Commit

---
+++ sharesChonse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sharesChonse
+//
+//  Created by 冯汉栩 on 2017/8/8.
+//  Copyright © 2017年 fenghanxuCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
